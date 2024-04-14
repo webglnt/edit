4d4a48e319234a556f544f4825f1dac7a7d680a9
Muffin <muffin@mailbox.org>
2024-04-13 19:12:39 -0500

    Partially revert "Implement getBlockly() callbacks in LazyScratchBlocks instead"
    Addons expect getBlockly() to resolve when the first workspace is created.

--- src/addons/api.js
+++ src/addons/api.js
@@ -19,7 +19,6 @@ import SettingsStore from './settings-store-singleton';
 import dataURLToBlob from '../lib/data-uri-to-blob';
 import EventTargetShim from './event-target';
 import AddonHooks from './hooks';
-import LazyScratchBlocks from '../lib/tw-lazy-scratch-blocks.js';
 import addons from './generated/addon-manifests';
 import addonMessages from './addons-l10n/en.json';
 import l10nEntries from './generated/l10n-entries';
@@ -191,7 +190,14 @@ class Tab extends EventTargetShim {
             get vm () {
                 return reduxInstance.state.scratchGui.vm;
             },
-            getBlockly: () => new Promise(resolve => LazyScratchBlocks.onLoaded(resolve)),
+            getBlockly: () => {
+                if (AddonHooks.blockly) {
+                    return Promise.resolve(AddonHooks.blockly);
+                }
+                return new Promise(resolve => {
+                    AddonHooks.blocklyCallbacks.push(() => resolve(AddonHooks.blockly));
+                });
+            },
             getWorkspace: () => AddonHooks.blocklyWorkspace,
             getPaper: async () => {
                 const modeSelector = await this.waitForElement("[class*='paint-editor_mode-selector']", {
