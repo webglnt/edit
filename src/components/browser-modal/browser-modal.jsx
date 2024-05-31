f280696b67b11c474abbf9d38c8d4fb8e457ad6a
KyleKart <71104484+KyleKart@users.noreply.github.com>
2024-05-31 20:45:15 -0300

    Update browser-modal.jsx

--- src/components/browser-modal/browser-modal.jsx
+++ src/components/browser-modal/browser-modal.jsx
@@ -1,8 +1,8 @@
-import { defineMessages, FormattedMessage, intlShape, injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import ReactModal from 'react-modal';
 import Box from '../box/box.jsx';
+import { defineMessages, injectIntl, intlShape, FormattedMessage } from 'react-intl';
 import { isRendererSupported, isNewFunctionSupported, findIncompatibleUserscripts } from '../../lib/tw-environment-support-prober.js';
 import styles from './browser-modal.css';
 import unhappyBrowser from './unsupported-browser.svg';
@@ -16,25 +16,13 @@ const messages = defineMessages({
     }
 });
 
-const LearnMore = props => (
-    <React.Fragment>
-        {' '}
-        <DocumentationLink {...props}>
-            <FormattedMessage
-                defaultMessage="Learn more."
-                id="gui.alerts.cloudInfoLearnMore"
-            />
-        </DocumentationLink>
-    </React.Fragment>
-);
-
 const BrowserModal = ({ intl, ...props }) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(true); // State to manage modal open/close
     const label = messages.label;
     const incompatibleUserscripts = findIncompatibleUserscripts();
 
     const handleClose = () => {
-        setIsOpen(false);
+        setIsOpen(false); // Function to close the modal
     };
 
     return (
@@ -43,27 +31,70 @@ const BrowserModal = ({ intl, ...props }) => {
             className={styles.modalContent}
             contentLabel={intl.formatMessage({ ...messages.label })}
             overlayClassName={styles.modalOverlay}
-            onRequestClose={handleClose}
+            onRequestClose={handleClose} // Set onRequestClose to handleClose
         >
-            <div dir={props.isRtl ? 'rtl' : 'ltr'}>
-                <Box className={styles.header}>
-                    <CloseButton size={CloseButton.SIZE_LARGE} onClick={handleClose} />
+            <Box dir={props.isRtl ? 'rtl' : 'ltr'}>
+                {/* Header section with close button */}
+                <div className={styles.header}>
+                    <CloseButton onClick={handleClose} className={styles.closeButton} />
                     <h2>
                         <FormattedMessage {...label} />
                     </h2>
-                </Box>
-
-                <Box className={styles.illustration}>
-                    <img
-                        src={unhappyBrowser}
-                        draggable={false}
-                    />
-                </Box>
+                </div>
 
                 <Box className={styles.body}>
-                    {/* Rest of the modal content */}
+                    <Box className={styles.illustration}>
+                        <img
+                            src={unhappyBrowser}
+                            draggable={false}
+                        />
+                    </Box>
+
+                    <Box className={styles.content}>
+                        {/* Main content goes here */}
+                        {/* eslint-disable max-len */}
+                        {isNewFunctionSupported() ? null : (
+                            // This message should only be seen by website operators, so we don't need to translate it
+                            <p>
+                                {'Unable to compile JavaScript with new Function(). This is most likely caused by an overly-strict Content-Security-Policy. The CSP must include \'unsafe-eval\'.'}
+                            </p>
+                        )}
+
+                        {incompatibleUserscripts.length > 0 && (
+                            <React.Fragment>
+                                {incompatibleUserscripts.map((message, index) => (
+                                    <p key={index}>
+                                        {message}
+                                    </p>
+                                ))}
+                            </React.Fragment>
+                        )}
+
+                        {!isRendererSupported() && (
+                            <React.Fragment>
+                                <p>
+                                    <FormattedMessage
+                                        defaultMessage="Your browser {webGlLink} which is usually needed for Scratch to run. But WebGLn't is designed to run without it!"
+                                        description="WebGL missing message. {webGLLink} is a link with the text 'does not support WebGL' from Scratch's translations"
+                                        id="tw.webglModal.description"
+                                        values={{
+                                            webGlLink: (
+                                                <a href="https://get.webgl.org/">
+                                                    <FormattedMessage
+                                                        defaultMessage="does not support WebGL"
+                                                        description="link part of your browser does not support WebGL message"
+                                                        id="gui.webglModal.webgllink"
+                                                    />
+                                                </a>
+                                            )
+                                        }}
+                                    />
+                                </p>
+                            </React.Fragment>
+                        )}
+                    </Box>
                 </Box>
-            </div>
+            </Box>
         </ReactModal>
     );
 };
@@ -73,4 +104,8 @@ BrowserModal.propTypes = {
     isRtl: PropTypes.bool
 };
 
-export default injectIntl(BrowserModal);
+const WrappedBrowserModal = injectIntl(BrowserModal);
+
+WrappedBrowserModal.setAppElement = ReactModal.setAppElement;
+
+export default WrappedBrowserModal;
