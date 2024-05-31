3539966066da8dc1af132869d77f58926d9c0668
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-05-31 12:55:56 -0700

    Update purple.js

--- src/lib/themes/accent/purple.js
+++ src/lib/themes/accent/purple.js
@@ -1,4 +1,5 @@
 const guiColors = {
+    'glosstex': 'url(./glossAmethyst.png)',
     'motion-primary': 'hsla(260, 60%, 60%, 1)',
     'motion-primary-transparent': 'hsla(260, 60%, 60%, 0.9)',
     'motion-tertiary': 'hsla(260, 42%, 51%, 1)',
