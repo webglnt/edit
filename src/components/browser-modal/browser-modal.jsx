328124a57f8c7d42064a6576826f68f3029b34a3
KyleKart <71104484+KyleKart@users.noreply.github.com>
2024-05-31 22:53:21 -0300

    Update browser-modal.jsx

--- src/components/browser-modal/browser-modal.jsx
+++ src/components/browser-modal/browser-modal.jsx
@@ -25,10 +25,67 @@ const BrowserModal = ({ intl, ...props }) => {
     const handleClose = () => {
         setIsOpen(false); // Function to close the modal
     };
+
+    return (
+    <Modal
+        isOpen
+        className={styles.modalContent}
+        onRequestClose={props.onClose}
+        contentLabel="Missing WebGL"
+        id="browserModal"
+        >
+            <Box dir={props.isRtl ? 'rtl' : 'ltr'}>
+                {/* Header section with close button */}
+                <div className={styles.header}>
+                </div>
+
+                <Box className={styles.body}>
+                        {/* Main content goes here */}
+                        {/* eslint-disable max-len */}
+                        {isNewFunctionSupported() ? null : (
+                            // This message should only be seen by website operators, so we don't need to translate it
+                            <p>
+                                {'Unable to compile JavaScript with new Function(). This is most likely caused by an overly-strict Content-Security-Policy. The CSP must include \'unsafe-eval\'.'}
+                            </p>
+                        )}
+
+                        {incompatibleUserscripts.length > 0 && (
+                            <React.Fragment>
+                                {incompatibleUserscripts.map((message, index) => (
+                                    <p key={index}>
+                                        {message}
+                                    </p>
+                                ))}
+                            </React.Fragment>
+                        )}
+
+                        {!isRendererSupported() && (
+                            <React.Fragment>
+                                <p>
+                                    <FormattedMessage
+                                        defaultMessage="Your browser {webGlLink} which is usually needed for Scratch to run. But WebGLn't is designed to run without it!"
+                                        description="WebGL missing message. {webGLLink} is a link with the text 'does not support WebGL' from Scratch's translations"
+                                        id="tw.webglModal.description"
+                                        values={{
+                                            webGlLink: (
+                                                <a href="https://get.webgl.org/">
+                                                    <FormattedMessage
+                                                        defaultMessage="does not support WebGL"
+                                                        description="link part of your browser does not support WebGL message"
+                                                        id="gui.webglModal.webgllink"
+                                                    />
+                                                </a>
+                                            )
+                                        }}
+                                    />
+                                </p>
+                            </React.Fragment>
+                        )}
+                    </Box>
+                </Box>
+        </Modal>
+    );
 };
- return (
-null
-     );
 
 BrowserModal.propTypes = {
     intl: intlShape.isRequired,
@@ -37,6 +94,6 @@ BrowserModal.propTypes = {
 
 const WrappedBrowserModal = injectIntl(BrowserModal);
 
-WrappedBrowserModal.setAppElement = ReactModal.setAppElement;
+WrappedBrowserModal.setAppElement = Modal.setAppElement;
 
 export default WrappedBrowserModal;
